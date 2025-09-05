86c09f4c0e3c93ba444e852db4b2c9ade18dce91
Hanumanthu Manoj <hmanoj.ece@gmail.com>
2025-09-05 20:53:15 +0530

    Reruning the pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from my DevOps Project!"
+    return " Hello from my DevOps Project!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
